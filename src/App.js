ef6ae678a2748ec6cfc64ae56641ec9231fedd28
İrem Telli <merveyirem@gmail.com>
2023-09-14 16:19:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bulma/css/bulma.css"
+import "./App.css"
+import Course from "./Course"
+import Angular from "./images/angular.jpg"
+import Bootsrap from "./images/bootstrap5.png"
+import Frontend from "./images/ccsharp.png"
+import Web from "./images/kompleweb.jpg"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="hero is-link">
+        <div class="hero-body">
+          <p class="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section"></section>
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Angular}
+              title="Angular  "
+              description="Her gün öğrenmeye biraz zaman ayırmak sonuç verir. Araştırmalara göre öğrenimi alışkanlık haline getiren öğrencilerin hedeflerine ulaşma olasılığı daha fazladır. Öğrenmeye zaman ayırın ve öğrenim planlayıcınızı kullanarak hatırlatmalar alın"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Bootsrap}
+              title="Bootstrap 5  "
+              description="Her gün öğrenmeye biraz zaman ayırmak sonuç verir. Araştırmalara göre öğrenimi alışkanlık haline getiren öğrencilerin hedeflerine ulaşma olasılığı daha fazladır. Öğrenmeye zaman ayırın ve öğrenim planlayıcınızı kullanarak hatırlatmalar alın"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Frontend}
+              title="Komple Web  "
+              description="Her gün öğrenmeye biraz zaman ayırmak sonuç verir. Araştırmalara göre öğrenimi alışkanlık haline getiren öğrencilerin hedeflerine ulaşma olasılığı daha fazladır. Öğrenmeye zaman ayırın ve öğrenim planlayıcınızı kullanarak hatırlatmalar alın"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Web}
+              title="Frontend "
+              description="Her gün öğrenmeye biraz zaman ayırmak sonuç verir. Araştırmalara göre öğrenimi alışkanlık haline getiren öğrencilerin hedeflerine ulaşma olasılığı daha fazladır. Öğrenmeye zaman ayırın ve öğrenim planlayıcınızı kullanarak hatırlatmalar alın"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Course.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+function Course({ image, title, description }) {
+  //   const title=props.title
+  //  const description=props.description
+  //   const { title, description } = props
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">{description}</div>
+      </div>
+    </div>
+    // <div>
+    //   <img src={image} alt="" />
+    //   <div>{title}</div>
+    //   <div>{description}</div>
+    // </div>
+  )
+}
+
+export default Course
